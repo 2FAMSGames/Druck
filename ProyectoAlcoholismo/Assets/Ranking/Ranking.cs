73fbd8ce597ed6fb9295e1ae0038544e9a9363a1
Felix de las Pozas Alvarez <felix.delaspozas@urjc.es>
2023-03-31 18:35:23 +0200

    Ranking scene WIP.

--- ProyectoAlcoholismo/Assets/Ranking/Ranking.cs
+++ ProyectoAlcoholismo/Assets/Ranking/Ranking.cs
@@ -1,7 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Color32 = UnityEngine.Color32;
 
 public class Ranking : MonoBehaviour
 {
@@ -12,14 +12,28 @@ public class Ranking : MonoBehaviour
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         VisualElement jugadoresUI = root.Q("Jugadores");
-
-        foreach (var jugador in jugadores)
+        
+        // TODO: get scores from GameState according to current game
+        var scores = PlayerRegistry.Instance.SortedScores();
+        // TODO: get current game name from GameState too.
+        //var gameName = GameState.currentGameName();
+        //var GameTitle = root.Q<Label>("NombreJuego").text = gameName;
+        
+        foreach (var player in scores)
         {
-            TemplateContainer jugadorContainer = jugadorTemplate.Instantiate();
-            jugadoresUI.Add(jugadorContainer);
-            root.Q<Label>("Nombre").text = jugador;
-            root.Q<Label>("Nombre").name = jugador;
+            var playerValues = GameState.GetPlayer(player.Item1);
+            var name = playerValues.playerName;
+            var color = playerValues.playerColor;
+            var score = playerValues.playerScore;
 
+            var styleColor = new StyleColor(new Color32((byte)(color[0] * 255),
+                                                                (byte)(color[1] * 255),
+                                                                (byte)(color[2] * 255), 255));
+            
+            TemplateContainer jugadorContainer = jugadorTemplate.Instantiate();
+            jugadorContainer.Q<Label>("Nombre").text = name;
+            jugadorContainer.Q<VisualElement>("Icono").style.color = styleColor;
+            jugadoresUI.Add(jugadorContainer);
         }
     }
 }
